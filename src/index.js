85b4e6eec88f0b00676a0e4a976317d527f84f4c
alvin <alvin@mabolotech.com>
2024-02-13 16:41:31 +0800

    add cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const user = require("./routers/user");
 const path = require("path");
+const cors = require("cors");
 require("dotenv").config({ path: path.join(__dirname, "config", ".env") });
 const connectToDatabase = require("./config/database");
 const app = express();
 app.use(express.json());
+app.use(cors());
 connectToDatabase();
 
 app.get("/", (req, res) => {
